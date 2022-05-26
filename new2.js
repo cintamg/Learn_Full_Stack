ec486b08536995a42739ca7d36ff345d2d223a26
cintamg <cintaelamthottam@gmail.com>
2022-05-26 21:57:53 +0530

    changes

---
+++ new2.js
@@ -0,0 +1,7 @@
+
+
+var student=[{"name":"Cinta","age":21,"mob":"8281952708"},
+{"name":"Ciya","age":21,"mob":"8281952789"},{"name":"Sarah","age":24,"mob":"828196589"}
+]
+console.log(student.length)
+
