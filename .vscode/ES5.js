aafd163c36f6d3304f728bb26820be0cc80b38f1
cintamg <cintaelamthottam@gmail.com>
2022-06-16 00:17:22 +0530

    ECMAScript 6 and some modifications

---
+++ .vscode/ES5.js
@@ -0,0 +1,14 @@
+//ECMAScript 6
+add=(a,b)=>{
+    c=a+b
+    return c;
+}
+// ECMAScript 5
+// function add(a,b){
+//     c=a+b
+//     return c;
+// }
+var x=23
+var y=22
+z=add(x,y)
+console.log(z)
